e2a7c84ed962c552654a1d1e034fa29c4069f3d2
iEduardoD3V <eduardo2677816@aluno.ba.senac.br>
2023-08-11 11:09:52 -0300

    inplementado js de quantidade

--- assets/javaScript/main.js
+++ assets/javaScript/main.js
@@ -1,31 +1,63 @@
-(function(){
+(function () {
+  const cep = document.querySelector("input[name=cep]");
 
-	const cep = document.querySelector("input[name=cep]");
-  
-  cep.addEventListener('blur', e=> {
-  		const value = cep.value.replace(/[^0-9]+/, '');
-      const url = `https://viacep.com.br/ws/${value}/json/`;
-      
-      fetch(url)
-      .then( response => response.json())
-      .then( json => {
-      		
-          if( json.logradouro ) {
-          	document.querySelector('input[name=rua]').value = json.logradouro;
-            document.querySelector('input[name=bairro]').value = json.bairro;
-            document.querySelector('input[name=cidade]').value = json.localidade;
-            document.querySelector('input[name=estado]').value = json.uf;
-          }
-      
+  cep.addEventListener("blur", (e) => {
+    const value = cep.value.replace(/[^0-9]+/, "");
+    const url = `https://viacep.com.br/ws/${value}/json/`;
+
+    fetch(url)
+      .then((response) => response.json())
+      .then((json) => {
+        if (json.logradouro) {
+          document.querySelector("input[name=rua]").value = json.logradouro;
+          document.querySelector("input[name=bairro]").value = json.bairro;
+          document.querySelector("input[name=cidade]").value = json.localidade;
+          document.querySelector("input[name=estado]").value = json.uf;
+        }
       });
-      
-      
   });
-
-	
-
-
-
-
-
 })();
+
+let count = 0;
+let precoTotal = parseInt(document.getElementById("precoTotal").value);
+// let precoTotal2 = document.getElementById("precoTotal").value;
+function btMais() {
+  count++;
+  document.getElementById("carrinhoItem").innerHTML = count;
+  if (count <= 0) {
+    document.getElementById("carrinhoItem").innerHTML = 0;
+  } else if (count >= 1) {
+    document.getElementById("precoTotal").innerHTML = `R$ ${1000 * count}`;
+  }
+}
+function btMenos() {
+  count--;
+  document.getElementById("carrinhoItem").innerHTML = count;
+  if (count <= 0) {
+    document.getElementById("carrinhoItem").innerHTML = 0;
+    document.getElementById("precoTotal").innerHTML = "R$ 0.00";
+  } else if (count >= 1) {
+    document.getElementById("precoTotal").innerHTML = `R$ ${1000 * count}`;
+  }
+}
+
+function btMais2() {
+  count++;
+  document.getElementById("carrinhoItem2").innerHTML = count;
+  if (count <= 0) {
+    document.getElementById("carrinhoItem2").innerHTML = 0;
+  } else if (count >= 1) {
+    document.getElementById("precoTotal").innerHTML = `R$ ${2000 * count}`;
+  }
+}
+
+function btMenos2() {
+  count--;
+  document.getElementById("carrinhoItem2").innerHTML = count;
+  if (count <= 0) {
+    document.getElementById("carrinhoItem2").innerHTML = 0;
+    document.getElementById("precoTotal").innerHTML = "R$ 0.00";
+  } else if (count >= 1) {
+    document.getElementById("precoTotal").innerHTML = `R$ ${2000 * count}`;
+  }
+}
